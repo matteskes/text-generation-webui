2343029db77acc30915cf0b51f8c50649ed52cc8
matteskes <matt@eskesfamily.com>
2025-07-24 23:25:28 -0700

    cleaning up stashed commits

--- extensions/openai/tokens.py
+++ extensions/openai/tokens.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from modules.text_generation import decode, encode
 
 
@@ -24,3 +25,42 @@ def token_decode(tokens):
     return {
         'text': output
     }
+=======
+from extensions.openai.utils import float_list_to_base64
+from modules.text_generation import encode, decode
+import numpy as np
+
+def token_count(prompt):
+    tokens = encode(prompt)[0]
+
+    return {
+        'results': [{
+            'tokens': len(tokens)
+        }]
+    }
+
+
+def token_encode(input, encoding_format):
+    # if isinstance(input, list):
+    tokens = encode(input)[0]
+
+    return {
+        'results': [{
+            'tokens': tokens,
+            'length': len(tokens),
+        }]
+    }
+
+
+def token_decode(tokens, encoding_format):
+    # if isinstance(input, list):
+    #    if encoding_format == "base64":
+    #         tokens = base64_to_float_list(tokens)
+    output = decode(tokens)[0]
+
+    return {
+        'results': [{
+            'text': output
+        }]
+    }
+>>>>>>> Stashed changes
--- extensions/whisper_stt/readme.md
+++ extensions/whisper_stt/readme.md
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # whisper_stt
 
 Allows you to enter your inputs in chat mode using your microphone.
@@ -13,3 +14,20 @@ whisper_stt-auto_submit: False
 ```
 
 See source documentation for [model names](https://github.com/openai/whisper#available-models-and-languages) and [languages](https://github.com/openai/whisper/blob/main/whisper/tokenizer.py) you can use.
+=======
+# whisper_stt
+
+Allows you to enter your inputs in chat mode using your microphone.
+
+## Settings
+
+To adjust your default settings, you can add the following to your settings.yaml file.
+
+```
+whisper_stt-whipser_language: chinese
+whisper_stt-whipser_model: tiny
+whisper_stt-auto_submit: False
+```
+
+See source documentation for [model names](https://github.com/openai/whisper#available-models-and-languages) and (languages)[https://github.com/openai/whisper/blob/main/whisper/tokenizer.py] you can use.
+>>>>>>> Stashed changes
